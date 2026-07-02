30f4fd35dc0d9a46b8a5be7e4cfd5eb654595b63
Fiza Jaleel <fizajaleel4072@gmail.com>
2026-07-02 01:42:19 -0700

    Improve code comments and update output filename

--- Quotes_Scraper.py
+++ Quotes_Scraper.py
@@ -13,20 +13,28 @@ data={
 for i in range(1,11):
     url=f"https://quotes.toscrape.com/page/{i}"
     try:
+        #Fetching html
+        
         res=requests.get(url)
         res.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"Request Failed to fetch page {i}: {e}")
         continue
 
+    #Parsing html with Beautifulsoup
+
     soup=BeautifulSoup(res.content, "html.parser")
+    
+    #Getting Quotes
 
     results=soup.find_all("span", class_="text")
 
     for result in results:
             print(result.get_text())
             data["Quote"].append(result.get_text())
-        
+
+    #Getting Author's 
+
     s=soup.find_all("small", class_="author")
     for results in s:
              print(results.get_text())
@@ -37,16 +45,11 @@ for i in range(1,11):
 
 # print(soup.prettify())
 
-# #For Saving in CSV Format
-
-# # df=pd.DataFrame(data)
-# # df.to_csv("Quotes.csv", index=False)
-
 
 #For Saving in Excel File
 df=pd.DataFrame(data)
 try:
-    df.to_excel("Quotes_Pagin_ation.xlsx", index=False)
+    df.to_excel("Quotes_Scraping.xlsx", index=False)
 except Exception as e:
     print("Could not save file", e)
 
